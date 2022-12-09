7f4f2527d85bc90617b57fb8b51edda309642f56
LuizCowBTF <luizcowbtf@gmail.com>
2022-12-08 21:02:09 -0300

    connections, databank, nodels and routes

---
+++ routes/jobs.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+
+router.get('/test', (req, res) => {
+   res.send("deu certo");
+});
+
+
+// add job via POST
+router.post('/add', (req, res) => {
+   let {title, description, salary, company, email, new_job} = req.body;
+
+   // INSERT
+   Job.create({title, description, salary, company, email, new_job})
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err));
+});
+
+module.exports = router;
